1972a049ebf27886e16fb964b46e2bbc596ba9a0
Khac Minh Le <khac.minh.le@outlook.de>
2020-04-15 16:38:16 +0200

    Updates

--- Data_Processing.bat
+++ Data_Processing.bat
@@ -1,4 +1,8 @@
 cd C:/Users/KhacM/GitHub/COVID-19
-git pull
+git pull https://github.com/CSSEGISandData/COVID-19
 
 py "C:/Users/KhacM/GitHub/COVID-19/DataProcessing.py"
+
+git add .
+git commit -m "Updates"
+git push
